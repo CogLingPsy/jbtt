fe09ff5b3873319b599df1aa991b3ac97eb1bfd4
Rhinrei <rhinrei@gmail.com>
2023-01-02 02:52:02 +0300

    add preprocessing: pos-tagging and tree creation from grammar

---
+++ src/processing/preprocessing.py
@@ -0,0 +1,75 @@
+from typing import List, Tuple
+
+import nltk
+import nltk.tree
+from nltk import Tree
+from src.processing.models.Preloading import Preloading
+from src.processing.models.Replace import Replace
+from src.processing.utils.preprocessing_utils import fix_pos_tagging, prepare_sentence_for_tagging
+
+
+def pos_tagging(phrase: str, preloading: Preloading) -> (List[Tuple[str, str]], List[Replace]):
+    """
+    Tags sentence with part-of-speech tags
+
+    :argument
+    phrase -- sentence to tag with POS-tags
+    :argument
+    preloading -- object containing dictionaries
+
+    :returns
+    list of tuples (word, tag) and list of replaces
+    """
+    tokens, replaces = prepare_sentence_for_tagging(phrase, preloading.contractions)
+    tags: List[Tuple[str, str]] = nltk.pos_tag(tokens)
+    fix_pos_tagging(tags, preloading.rules_fix_pos_tagging)
+
+    # add tag IT, because there is need in grammar to differ pronouns between he-she and it
+    same_word_tags: List[str] = ['it']
+    tags: List[Tuple[str, str]] = [
+        (w, w.upper()) if w in same_word_tags else (w, t)
+        for w, t in tags
+    ]
+
+    # enrich with Named Entity tag
+    entity_tags: Tree = nltk.ne_chunk(tags, binary=True)
+    entity_index = []
+    for chunk in entity_tags:
+        if hasattr(chunk, 'label'):
+            if not chunk[0]:  # nltk.ne_chunk counts every word with capital letter as NE, so that first word is excluded
+                entity_index.append(entity_tags.index(chunk))
+    if len(entity_index) != 0:
+        for index in entity_index:
+            tags[index] += ('NE',)
+    return tags, replaces
+
+
+def create_tree_from_grammar(tags: Tree) -> Tree:
+    """
+    Extracts noun groups according to grammar by Regexp rules
+
+    :argument
+    tags -- list of tuples (word, tag)
+
+    :returns
+    tree with indicated grammar groups
+    """
+    pattern: str = """
+                EXIND: {<DT|EX|IT>+<VBZ|VBD>+<DT>?<JJ>*<NN>+}
+                VBPART: {<VBZ|VBD><NN>+<IN>+}
+                NPP: {<DT>+<NN>?<IN>?<CD>?<DT|PRP\$>?<JJS|JJ>*<NN|NNS>+}
+                NP: {<CD>?<DT|PRP\$>?<JJS|JJ>*<NN|NNS>+}
+                """
+    cp: nltk.RegexpParser = nltk.RegexpParser(pattern)
+    result: Tree = cp.parse(tags)
+
+    # mark unmarked leafs with OTHER to process them further as trees and count symbols in them
+
+    for index, element in enumerate(result):
+        if not isinstance(element, Tree):
+            if isinstance(element, Tuple):
+                element = f"(OTHER {element[0]}/{element[1]})"
+            result[index] = Tree.fromstring(str(element))
+    tree: Tree = Tree.fromstring(str(result))
+
+    return tree
---
+++ src/processing/utils/preprocessing_utils.py
@@ -0,0 +1,109 @@
+import hashlib
+from typing import Dict, List, Tuple
+
+from src.processing.models.Replace import Replace
+from nltk import sent_tokenize
+
+def fix_pos_tagging(tags: List[Tuple[str, str]], rules: List[Dict]) -> List[Dict]:
+    """
+    Loads a dictionary of words which can have wrong POS tag after tagging.
+    Changes wrong tag for word in list of tuples of noun-tag
+
+    Example actor is recognized by nltk as adjective, meanwhile it's a noun.
+
+    :argument
+    tags -- a list with tuples word-tag
+
+    :argument
+    rules --dictionary of rules to fix wrong POS tagging from nltk
+
+    :returns
+    List of rules to change tags
+    """
+    for tag in tags:
+        for rule in rules:
+            if rule['word'] == tag[0].lower() and tag[1] == rule['posTag']:
+                index = tags.index(tag)
+                tags[index] = (tag[0], rule['changeTo'])
+    return rules
+
+
+def prepare_sentence_for_tagging(sentence: str, contractions: Dict[str, str]) -> (List[str], List[Replace]):
+    """
+    Attention! Changes the initial sentence
+    Applies contractions from contractions dictionary to sentence
+    Changes apostrophe, ), ( as they affect parsing sentence to tree
+
+    :argument
+    sentence -- a sentence to change
+
+    :argument
+    contractions -- a dictionary of contractions of type it's -> it is
+
+    :returns
+    Changed sentence (i.e list of tokens), list of replaces made
+    """
+    symbols = ['(', ')', '/', '}', '{', '.', ',', ":", ";", "?", '!']
+    replaces: List[Replace] = []
+    replaced_sentence_tokens = []
+
+    for index, word in enumerate(sentence.split()):
+        unified_word = word.lower().replace("`", "'").replace("’", "'")
+        rep: Replace
+        has_changed_symbols = False
+        for symbol in symbols:
+            if symbol in unified_word:
+                rep = Replace(len(replaced_sentence_tokens), 1, word)
+                if symbol == '/':
+                    unified_word = unified_word.replace('/', 'or')
+                if symbol == '}':
+                    unified_word = unified_word.replace('}', 'and')
+                if symbol == '{':
+                    unified_word = unified_word.replace('{', 'and')
+                else:
+                    unified_word = unified_word.translate({ord(ch): '' for ch in symbols})
+                has_changed_symbols = True
+                break
+
+        if unified_word in contractions.keys():
+            replace_words = contractions[unified_word].split()
+            rep = Replace(len(replaced_sentence_tokens), len(replace_words), word)
+            replaces.append(rep)
+            replaced_sentence_tokens += replace_words
+        else:
+            if has_changed_symbols:
+                replaces.append(rep)
+                replaced_sentence_tokens.append(unified_word)
+            else:
+                replaced_sentence_tokens.append(word)
+    return replaced_sentence_tokens, replaces
+
+
+def md5(string):
+    """
+    :returns
+    hash of a sentence to save as cache key
+    """
+    return hashlib.md5(string.encode('utf-8')).hexdigest()
+
+
+def get_sentence_tokens(text: str) -> List[str]:
+    """
+    Tokenizes a text to sentence tokens
+
+    :argument
+    text -- text to tokenize
+
+    :returns
+    tokens
+    """
+    lines = text.split('\n')
+    tokenized = []
+    for line in lines:
+        if line == '':
+            tokenized.append(line)
+        else:
+            tokenized += sent_tokenize(line)
+            tokenized.append('')
+    tokenized.pop(len(tokenized) - 1)
+    return tokenized

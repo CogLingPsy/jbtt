4efbffe7a7a3cbcb82486803a1bcafc1c9c4193e
Rhinrei <rhinrei@gmail.com>
2023-01-02 02:54:40 +0300

    add web and cli applications

---
+++ src/applications/app.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+from flask import Flask, request, jsonify, redirect
+
+current: str = os.path.dirname(os.path.realpath(__file__))
+parent: str = os.path.dirname(current)
+grandparent: str = os.path.dirname(parent)
+sys.path.append(grandparent)
+from src.processing.run_processing import process_text
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+
+@app.route('/')
+def main_page():
+    return redirect("http://127.0.0.1:5000/static/index.html", code=302)
+
+
+@app.post('/processText')
+def post_process_text():
+    """
+    Endpoint to process text e.g. in Postman
+    """
+    data = request.json
+    result = process_text(data['text'])
+    return jsonify(result)
+
+
+@app.get('/alive')
+def get_status():
+    return jsonify('Active')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ src/applications/cli.py
@@ -0,0 +1,45 @@
+import os
+import sys
+from typing import IO
+
+import click
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+grandparent = os.path.dirname(parent)
+sys.path.append(grandparent)
+
+from src.processing.run_processing import process_text
+
+
+@click.command()
+@click.option('--text', help='')
+@click.option('--input_file', type=click.File('r', encoding='utf-8'))
+def cli_text(text: str, input_file: IO):
+    """
+    Cli app to run processing from terminal
+    python src/applications/cli.py --text="It’s beautiful city. He is actor."
+    python src/applications/cli.py --input_file=resources/moby_dick
+    """
+    if input_file:
+        text: str = input_file.read()
+    answer = process_text(text, True)
+
+    for results in answer:
+        sentence = results['text']
+        if not results['suggestions']:
+            click.secho(f"Sentence '{sentence}' is completely fine!", fg='green')
+        else:
+            click.secho(f"Can suggest improvements for '{sentence}'", fg='blue')
+            for suggestion in results['suggestions']:
+                click.secho(f"Cause: '{suggestion['cause']}'", fg='blue')
+                click.secho(sentence[0:suggestion['start']], nl=False)
+                click.secho(sentence[suggestion['start']:suggestion['end']], fg='red', nl=False)
+                click.secho(sentence[suggestion['end']:-1])
+                click.secho(f"Suggestions: ", nl=False)
+                click.secho("; ".join(suggestion['replacements']), fg='green')
+            click.echo('\n')
+
+
+if __name__ == '__main__':
+    cli_text()
